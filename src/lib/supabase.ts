91a815f23a72fa448af93b17406103c09507babc
alexmn238 <alexmn238@gmail.com>
2026-09-23 22:55:28 +0200

    Fix supabase import path for Vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || https://uhaheixapuowzyqlddcg.supabase.co;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || sb_publishable_zXwX_d0hXmg7HX4fr3Eshg_MDOA7Fzn;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
